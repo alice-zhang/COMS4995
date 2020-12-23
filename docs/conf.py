e42da869e39883ba97a7347957561c04ca14e787
alice-zhang <ayz2105@columbia.edu>
2020-12-22 18:19:02 -0800

    Adds autodocumentation

--- docs/conf.py
+++ docs/conf.py
@@ -31,8 +31,9 @@ release = '0.0.1'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['recommonmark']
+extensions = ['recommonmark', 'autoapi.extension']
 source_suffix = ['.rst', '.md']
+autoapi_dirs = ['../../mates']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
